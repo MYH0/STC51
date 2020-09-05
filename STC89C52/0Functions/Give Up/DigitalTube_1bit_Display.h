521f15611a32c3b50b6a4cb7fdb4dc2d0e95160c
MYH0 <62194572+MYH0@users.noreply.github.com>
2020-09-05 12:35:51 +0800

    Delete DelayMs.h

---
+++ STC89C52/0Functions/Give Up/DigitalTube_1bit_Display.h
@@ -0,0 +1,18 @@
+sbit wx = P2 ^ 7;
+sbit dx = P2 ^ 6;
+
+uchar code duanxuan[] = { 0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,
+0x77,0x7c,0x39,0x5e,0x79,0x71 };
+uchar code weixuan[] = { 0xfe,0xfd,0xfb,0xf7,0xef,0xdf };
+
+void DigitalTube_1bit_Display(uchar duan, uchar wei)     //段数据，位数据
+{
+	dx = 1;
+	P0 = duanxuan[duan];   //送段数据
+	dx = 0;
+	P0 = 0xff;		//送位数据前关闭所有显示，防止打开位选锁存后段选数据通过位选锁存器
+	wx = 1;
+	P0 = weixuan[wei];
+	wx = 0;
+	DelayMs(1);
+}
